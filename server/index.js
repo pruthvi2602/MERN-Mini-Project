242ae7348fcca9b7d88c1d5425dccb417e8a7919
Pruthvi Buhehcha <155176538+pruthvi2602@users.noreply.github.com>
2024-03-29 09:40:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://nandan:nandan1234@nandan.gnlqsjc.mongodb.net/?retryWrites=true&w=majority&appName=nandan';
+// add connection string for mongodb
+const CONNECTION_URL = '    ';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
